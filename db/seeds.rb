8c47a1320a7734b27487ec75bc241679b068f4b9
Antonio Djigo <antonio@secret-source.eu>
2020-11-12 16:39:16 +0000

    :seedling: Faker update to create less data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
 10.times do
-10.times do
-    User.create ({
-        email: Faker::Internet.email,
-        password: Faker::Internet.password
-    })
+  User.create ({
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  })
 
-    Customer.create ({
-        name: Faker::JapaneseMedia::Doraemon.character,
-        surname: Faker::Creature::Horse.name,
-        created_by: User.find(User.pluck(:id).sample).email
-    })
-    end
+  Customer.create ({
+    name: Faker::JapaneseMedia::Doraemon.character,
+    surname: Faker::Creature::Horse.name,
+    created_by: User.find(1).email
+  })
 end
