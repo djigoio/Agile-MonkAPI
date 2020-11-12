2e7e2c4c0c2819167aba88c4f4b1236b76d83e65
Antonio Djigo <antonio@secret-source.eu>
2020-11-12 15:10:20 +0000

    :seedling: Add faker content for users and customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+10.times do
+    User.create ({
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+    })
+
+    Customer.create ({
+        name: Faker::JapaneseMedia::Doraemon.character,
+        surname: Faker::Creature::Horse.name,
+        created_by: User.find(User.pluck(:id).sample).email
+    })
+    end
+end
